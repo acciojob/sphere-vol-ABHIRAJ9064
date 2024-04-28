a5a2783d93954c24e77de3f05a0f02f9bd9b7ba6
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-04-28 09:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function volume_sphere() {
     //Write your code here
-function volume_sphere() {
-    //Write your code here
-    let radius = document.getElementById("radius").value;
-   let volume =  (4/3) * Math.PI * Math.pow(radius, 3);
-	volume = volume.toFixed(4);
-     document.getElementById("volume").value = volume
-    // console.log(radius);
+    let r = document.getElementById("radius").value;
+	let v = document.getElementById("volume");
 
-    return false;
-     
-    
+	let num1 = (4/3) * Math.PI * Math.pow(r, 3);
+	let num2=num1.toFixed(4);
+	v.value = num2;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
